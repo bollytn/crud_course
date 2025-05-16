9e0284f0c52dcff7d58957fc65d7e7215ea23911
mahjoubi.bilel <mahjoubi.bilel@gmail.com>
2025-05-16 13:27:45 +0000

    error NabBar suntaxe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
