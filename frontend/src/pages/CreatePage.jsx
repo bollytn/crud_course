2f14ce82981ac1943fc09b530531692070eb3bc8
unknown <mahjoubi.bilel@gmail.com>
2025-05-17 12:15:21 +0100

    gitignore file

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -35,6 +35,7 @@ const CreatePage = () => {
 
     return (
         <Container maxW={"container.sm"}>
+
             <VStack spacing={8}>
                 <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>Create Product</Heading>
             </VStack>
@@ -66,7 +67,9 @@ const CreatePage = () => {
                         onClick={handleAddProduct}
                     >Create Product</Button>
                 </VStack>
+
             </Box>
+            
         </Container>
     )
 }
